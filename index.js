05b6395f5efa95896568920dc46bef1868f5dfd2
Fajar krisna Jaya <fajarkrisnajayanti@gmail.com>
2024-12-22 01:00:35 +0700

    game root in index.js fixes

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-require ('dotenv').config();
+require('dotenv').config();
 const cors = require('cors');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const userRouter = require('./routers/users.router');
 const transactionRouter = require('./routers/transactions.router');
+const gameRouter = require('./routers/games.router'); // Add this line
 
 const app = express();
 const port = process.env.APP_PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(userRouter);
 app.use(transactionRouter);
+app.use(gameRouter); // Add this line
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
