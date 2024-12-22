d50b1fcb98600cfc079cf77092a2332dd3225c93
Fajar krisna Jaya <fajarkrisnajayanti@gmail.com>
2024-12-22 15:33:03 +0700

    hotfix fullname 3

--- repositories/users.repository.js
+++ repositories/users.repository.js
@@ -22,18 +22,19 @@ const findUserByEmail = async (email) => {
 
 
 const createUser = async (user) => {
-  const { email, username, fullname, password, avatar_url } = user;
+  const { email, username, password, avatar_url } = user;
 
   try {
     const result = await pool.query(
-      "INSERT INTO users (email, username, fullname, password, avatar_url) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-      [email, username, fullname || null, password, avatar_url] // Ensure fullname can be null
+      "INSERT INTO users (email, username, password, avatar_url) VALUES ($1, $2, $3, $4) RETURNING *",
+      [email, username, password, avatar_url]
     );
     return result.rows[0];
   } catch (error) {
     throw new Error("Database error occurred while creating the user.");
   }
 };
+
 
 const findTransactionById = async (id) => {
   try {
