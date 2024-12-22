7921b5e2443b8706bd9cbf69249ecff514db0050
Fajar krisna Jaya <fajarkrisnajayanti@gmail.com>
2024-12-22 17:15:26 +0700

    fix wincount and tie

--- repositories/games.repository.js
+++ repositories/games.repository.js
@@ -23,10 +23,10 @@ const createSinglePlayerGame = async (gameData) => {
 };
 
 const updateGame = async (gameId, gameData) => {
-  const { Player1_choice } = gameData;
+  const { Player1ID, Player1_choice } = gameData;
   const botChoices = ['rock', 'paper', 'scissors'];
   const Player2_choice = botChoices[Math.floor(Math.random() * botChoices.length)];
-  const WinnerID = determineWinner(Player1_choice, Player2_choice);
+  const WinnerID = determineWinner(Player1ID, 9999, Player1_choice, Player2_choice);
   const result = await pool.query(
     `UPDATE "public"."Game" SET "Player1_choice" = $1, "Player2_choice" = $2, "WinnerID" = $3 WHERE "GameID" = $4 RETURNING *`,
     [Player1_choice, Player2_choice, WinnerID, gameId]
@@ -44,17 +44,17 @@ const getGameById = async (id) => {
   return result.rows[0];
 };
 
-const determineWinner = (player1Choice, player2Choice) => {
+const determineWinner = (Player1ID, Player2ID, player1Choice, player2Choice) => {
   if (player1Choice === player2Choice) {
-    return null; // It's a tie
+    return "tie"; // It's a tie
   } else if (
     (player1Choice === 'rock' && player2Choice === 'scissors') ||
     (player1Choice === 'scissors' && player2Choice === 'paper') ||
     (player1Choice === 'paper' && player2Choice === 'rock')
   ) {
-    return player1ID; // Player1 wins
+    return Player1ID; // Player1 wins
   } else {
-    return 9999; // Player2 (bot) wins
+    return Player2ID; // Player2 (bot) wins
   }
 };
 
