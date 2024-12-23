a7d2774212cdf85036e4d6419498be7851f51ee6
Fajar krisna Jaya <fajarkrisnajayanti@gmail.com>
2024-12-23 13:16:19 +0700

    fix index leaderboard

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
-const router = express.Router();
+const bodyParser = require('body-parser');
 
-const leaderboardController = require('../controllers/leaderboard.controller');
+const userRouter = require('./routers/users.router');
+const transactionRouter = require('./routers/transactions.router');
+const gameRouter = require('./routers/games.router');
+const leaderboardRouter = require('./routers/leaderboard.router'); // Add this line
 
-router.get('/leaderboard', leaderboardController.getLeaderboard);
+const app = express();
+const port = process.env.APP_PORT;
 
-module.exports = router;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(userRouter);
+app.use(transactionRouter);
+app.use(gameRouter);
+app.use(leaderboardRouter); // Add this line
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
