7e627e83f3c67ca6313e9222487121f7b1a4564a
nawfalfudhayl <117434460+nawfalfudhayl@users.noreply.github.com>
2024-12-24 15:53:09 +0700

    Update leaderboard.repository.js

--- repositories/leaderboard.repository.js
+++ repositories/leaderboard.repository.js
@@ -1,16 +1,18 @@
-const pool = require('../db/db');
+const pool = require("../db/db");
 
 const getLeaderboard = async () => {
   const result = await pool.query(`
-    SELECT DISTINCT ON (user_id)
-      leaderboard_id,
-      user_id,
-      rank,
-      winrate,
-      last_played
-      FROM leaderboard
-      WHERE user_id != 9999
-      ORDER BY user_id, winrate DESC;
+  SELECT DISTINCT ON (l.user_id)
+  l.leaderboard_id,
+  l.user_id,
+  u.username,
+  l.rank,
+  l.winrate,
+  u.games_count AS last_played
+  FROM leaderboard l
+  JOIN users u ON l.user_id = u.id
+  WHERE l.user_id != 9999
+  ORDER BY l.user_id, l.winrate DESC
 
   `);
   return result.rows;
