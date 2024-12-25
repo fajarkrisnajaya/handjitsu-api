9da776fbd72bc41a40433c7856801e4adc25ad8f
Fajar krisna Jaya <fajarkrisnajayanti@gmail.com>
2024-12-26 00:34:58 +0700

    fix winrate

--- repositories/leaderboard.repository.js
+++ repositories/leaderboard.repository.js
@@ -1,18 +1,19 @@
-const pool = require("../db/db");
+const pool = require('../db/db');
 
 const getLeaderboard = async () => {
   const result = await pool.query(`
-  SELECT DISTINCT ON (l.user_id)
-  l.leaderboard_id,
-  l.user_id,
-  u.username,
-  l.rank,
-  l.winrate,
-  u.games_count AS last_played
-  FROM leaderboard l
-  JOIN users u ON l.user_id = u.id
-  WHERE l.user_id != 9999
-  ORDER BY l.user_id, l.winrate DESC
+    SELECT
+    u.username,
+    CAST(CAST(REPLACE(l.winrate, '%', '') AS DECIMAL(5,2)) AS CHAR(5)) || '%' AS winrate_text,
+    l.last_played
+FROM
+    users u
+JOIN
+    leaderboard l ON u.id = l.user_id
+WHERE
+    u.id != 9999
+ORDER BY
+    CAST(REPLACE(l.winrate, '%', '') AS DECIMAL(5,2)) DESC;
 
   `);
   return result.rows;
