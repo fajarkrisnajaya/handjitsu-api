eaf3706ecd5d894fada77064d69663e552c62d36
Fajar krisna Jaya <fajarkrisnajayanti@gmail.com>
2024-12-26 00:45:02 +0700

    fix winrate2 , rejoin table

--- repositories/leaderboard.repository.js
+++ repositories/leaderboard.repository.js
@@ -2,19 +2,26 @@ const pool = require('../db/db');
 
 const getLeaderboard = async () => {
   const result = await pool.query(`
-    SELECT
+   SELECT
     u.username,
-    CAST(CAST(REPLACE(l.winrate, '%', '') AS DECIMAL(5,2)) AS CHAR(5)) || '%' AS winrate_text,
-    l.last_played
+    CAST(CAST(REPLACE(l.winrate, '%', '') AS DECIMAL(5,2)) AS CHAR(5)) || '%' AS winrate_text
 FROM
     users u
 JOIN
-    leaderboard l ON u.id = l.user_id
+    (
+        SELECT
+            user_id,
+            winrate,
+            CAST(CAST(REPLACE(winrate, '%', '') AS DECIMAL(5,2)) AS CHAR(5)) || '%' AS winrate_text,
+            ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY last_played DESC) AS rn
+        FROM
+            leaderboard
+    ) l ON u.id = l.user_id
 WHERE
     u.id != 9999
+    AND l.rn = 1
 ORDER BY
     CAST(REPLACE(l.winrate, '%', '') AS DECIMAL(5,2)) DESC;
-
   `);
   return result.rows;
 };
